5c15c68aa6b6a3326c0ade3f9420a3bc375bfb9e
Djhekto <113421858+Djhekto@users.noreply.github.com>
2022-09-20 11:40:05 +0300

    Add files via upload
    chet napical
    ne znau python voobshe
    vot bi pisat1 na c++|rust

--- 2009Khametov/3.py
+++ 2009Khametov/3.py
@@ -4,7 +4,14 @@
 
 #func1   mod 50 ==0
 #func2   mod 8  ==0
+def func1(a,otve,bykva):
+	if (a%50 == 0): otve=otve+bykva
+	else	      :{ otve=otve+"-"+bykva
+			func1(a-25,otve,"B") }
 
+def func2(a,otve):
+	if (a%8 == 0): otve=otve+"+C"
+	else	     : otve=otve+"-C":
 #func1 chisl
 #  |true => A
 #  |false =>-A func1 chisl-25
@@ -12,3 +19,10 @@
 #		|False => -B
 #func2 chisl  |+C
 #	      |-C
+otvet=""
+b=input("vvedi chislo")
+func1(b,otvet,"A")
+func2(b,otvet)
+
+print(otvet)
+
