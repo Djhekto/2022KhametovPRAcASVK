0a8e1ce21d16e7097b70716889f41a52a3e1c940
Djhekto <113421858+Djhekto@users.noreply.github.com>
2022-09-20 11:48:17 +0300

    Add files via upload

--- 2009Khametov/3.py
+++ 2009Khametov/3.py
@@ -6,12 +6,13 @@
 #func2   mod 8  ==0
 def func1(a,otve,bykva):
 	if (a%50 == 0): otve=otve+bykva
-	else	      :{ otve=otve+"-"+bykva
-			func1(a-25,otve,"B") }
+	else	      : 
+		otve=otve+"-"+bykva
+		func1(a-25,otve,"B") 
 
 def func2(a,otve):
-	if (a%8 == 0): otve=otve+"+C"
-	else	     : otve=otve+"-C":
+	if (a%8 == 0): otve="".join([otve,"+C"])
+	else	     : otve="".join([otve,"-C"])
 #func1 chisl
 #  |true => A
 #  |false =>-A func1 chisl-25
