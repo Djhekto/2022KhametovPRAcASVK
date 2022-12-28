b1d43e4029d643e7f7e8e1ca3a72f6092f90eea1
Djhekto <113421858+Djhekto@users.noreply.github.com>
2022-12-28 13:26:37 +0300

    Add files via upload

--- 20221025/1/prog.py
+++ 20221025/1/prog.py
@@ -1,23 +1,15 @@
-#числа Фибоначчи - последовательность, начинающаяся с 1, 1, 
-# и продолжающаяся так, что очередной элемент равен сумме двух предшествующих элементов.
-#написать генератор-функцию fib(m, n),
-# возвращающую итератор по числа Фибоначчи, начинаю с m-го и заканчивая n-м (m⩽n), в нумерации с 0
+def fib(m,n):
+    x = 0
+    y = 1
+    for _ in range(m):
+        try:
+            x,y=y,x+y
+        except:
+            pass
+        
+    for _ in range(m,m+n):
+        yield y
+        x,y=y,x+y
 
-from itertools import islice
-
-def fgen():
-    a, b = 1, 1
-    yield a
-    while 1:
-        c = a + b
-        a = b 
-        b = c
-        yield a
-
-def fib(m, n):#https://stackoverflow.com/questions/5234090/how-to-take-the-first-n-items-from-a-generator-or-list
-    nelem = islice(fgen(), n+1)
-    lelem = [elem for elem in nelem if elem>=m]
-    return lelem
-
-a1, b1 = eval(input())
-print (*fib (a1, b1), sep = ", ")
+import sys
+exec(sys.stdin.read())
