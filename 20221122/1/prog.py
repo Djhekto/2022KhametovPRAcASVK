b5a6e99efd14c501a539d677afa6fe532552bf3e
Djhekto <113421858+Djhekto@users.noreply.github.com>
2022-12-29 19:51:55 +0300

    Add files via upload

--- 20221122/1/prog.py
+++ 20221122/1/prog.py
@@ -6,7 +6,7 @@ length = file[0]
 txt = file[1:]
 tail = len(file[1:])
 ii=1
-sections = sorted([txt[i*(int(tail/length)):(i+ii)*(int(tail/length))] \
+sections = sorted([txt[int(i*tail/length):int((i+ii)*(tail/length))] \
                    for i in range(length)])
 
 sys.stdout.buffer.write(bytes([length]) + b''.join(sections))
