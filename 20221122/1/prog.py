4072d4056b147116a67c59b045a6f351a60dc702
Djhekto <113421858+Djhekto@users.noreply.github.com>
2022-12-30 01:43:27 +0300

    Add files via upload

--- 20221122/1/prog.py
+++ 20221122/1/prog.py
@@ -6,7 +6,12 @@ length = file[0]
 txt = file[1:]
 tail = len(file[1:])
 ii=1
-sections = sorted([txt[int(i*tail/length):int((i+ii)*(tail/length))] \
+sections = sorted([txt[int(i*tail/length):int((i+ii)*tail/length)] \
                    for i in range(length)])
 
-sys.stdout.buffer.write(bytes([length]) + b''.join(sections))
+#lsections = list(sections)#.remove(b"\n")
+#print("\n",lsections)
+#print("\n",sections,"\n")
+sys.stdout.buffer.write((bytes([length]) + b''.join(sections))[:1] +
+                        (bytes([length]) + b''.join(sections))[3:])
+#sys.stdout.buffer.write(bytes(length) + b"".join(sorted(sections)))
