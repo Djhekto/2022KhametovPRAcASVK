023471ec96987608b751108828466580e0025dea
Djhekto <113421858+Djhekto@users.noreply.github.com>
2022-12-30 02:45:04 +0300

    Add files via upload

--- 20221011/3/prog.py
+++ 20221011/3/prog.py
@@ -1,43 +1,28 @@
-container = []
-water = 0
-gas = 0
-diagram_width = 20
+import sys
+from math import ceil
 
-try:
-    while line := input():
-        container.append([*line])
-        water += container[-1].count('~')
-        gas += container[-1].count('.')
-except EOFError:
-    pass
 
-w = len(container)
-h = len(container[0])
-volume = (w - 2) * (h - 2)
-t_container = [['#'] * w for i in range(h)]
-waterc = int((water*0)+1)
-gas -= gas % (w - 2)
-water = volume - gas
-filled_gas = 0
-for i in range(waterc, h - 1):
-    for j in range(waterc, w - 1):
-        if gas > filled_gas:
-            t_container[i][j] = '.'
-            filled_gas += waterc
-        if gas<= filled_gas:
-            t_container[i][j] = '~'
-for i in range(h):
-    print(''.join(t_container[i]))
-try:
-    align = len(str(water)) if water > gas else len(str(gas))
-except:
-    pass
-if water > gas:
-    water_part = diagram_width
-    gas_part = round(diagram_width * gas / water)
-if water <= gas:
-    water_part = round(diagram_width * water / gas)
-    gas_part = diagram_width
-
-print(f'{"." * gas_part:20}', f'{gas:{align}}/{volume}')
-print(f'{"~" * water_part:20}', f'{water:{align}}/{volume}')
+s = sys.stdin.read()
+k_w = s.count('~')
+k_g = s.count('.')
+s = list(map(list, s.split()))
+w = len(s)
+h = len(s[0])
+twenty = 20
+n_w = ceil(k_w / (w - 2))
+n_g = h - 2 - n_w
+hc = h
+print('#' * w)
+for i in range(hc - 2):
+    if i < n_g:
+        print('#' + '.' * (w - 2) + '#')
+    elif i>= n_g:
+        print('#' + '~' * (w - 2) + '#')
+print('#' * w)
+length = len(str(k_g + k_w) + str(max(k_g, k_w))) + 1
+if k_g >= k_w:
+    print(("." * twenty).ljust(twenty), f"{k_g}/{k_g + k_w}".rjust(length))
+    print(("~" * round(k_w / k_g * twenty)).ljust(twenty), f"{k_w}/{k_g + k_w}".rjust(length))
+elif k_g < k_w:
+    print(("." * round(k_g / k_w * twenty)).ljust(twenty), f"{k_g}/{k_g + k_w}".rjust(length))
+    print(("~" * twenty).ljust(twenty), f"{k_w}/{k_g + k_w}".rjust(length))
